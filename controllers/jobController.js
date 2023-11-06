05f50754cd58c2cb7c53caf8a2ee2855cabc0b0f
wb420 <82988726+wb420@users.noreply.github.com>
2023-11-06 14:49:26 +0600

    Update jobController.js

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -85,8 +85,32 @@ const updateJob = async (req, res) => {
   }
 };
 
+// --Apply to a a specific  job by id --
+const applyToJob = async (req, res) => {
+  const jobId = req.params.id;
+  const { applicantName, resume, email, phone, address } = req.body;
+
+  try {
+    const job = await Job.findById(jobId);
+
+    if (!job) {
+      return res.status(404).json({ message: 'Job not found' });
+    }
+
+    //--Adding applicant's name, resume, and contact information to the applicants array
+    job.applicants.push({ applicantName, resume, email, phone, address  });
+
+    // --Saveing the updated job document in the database--
+    const savedJob = await job.save();
+    res.status(201).json(savedJob);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+
 module.exports = {
   createJob,
   getJobs,
-  updatedJob,
-};
+  updateJob,
+}; 
