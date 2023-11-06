61357834c6bc51e0a8ccd5553bca725d9af9c9b1
wb420 <82988726+wb420@users.noreply.github.com>
2023-11-06 16:44:01 +0600

    Update jobController.js

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,4 +1,6 @@
 const Job = require('../models/jobModel');
+const JobApplication = require('../models/jobApplicationModel');
+
 
 // --Create a new job--
 const createJob = async (req, res) => {
@@ -14,7 +16,7 @@ const createJob = async (req, res) => {
     salary,
     experience,
     tags,
-    applicants: [],
+    
   });
 
   try {
@@ -34,6 +36,90 @@ const getJobs = async (req, res) => {
   // Create a filter object based on provided query parameters
   if (title) filter.title = new RegExp(title, 'i');
   if (location) filter.location = new RegExp(location, 'i');
+  if (company) filter.company = new RegExp(company, 'i');
+  if (tags) filter.tags = { $in: tags.split(',') };
+  if (experience) filter.experience = { $gte: parseInt(experience) };
+
+  try {
+    let query;
+
+    // Check if any filter parameters are provided
+    if (Object.keys(filter).length === 0) {
+      // No query parameters provided, fetch all jobs
+      query = Job.find();
+    } else {
+      // Query parameters provided, fetch data according to query parameter
+      query = Job.find(filter);
+    }
+
+    // Apply sorting based on the 'sort' query parameter
+    if (sort === 'newest') {
+      query.sort({ updatedAt: -1 });
+    } else if (sort === 'highest-paying') {
+      query.sort({ salary: -1 });
+    }
+
+
+    const jobs = await query.exec()
+   console.log(jobs)
+    res.json(jobs);
+  } catch (err) { 
+    console.log("error bal")
+    res.status(500).json({ message: err.message });
+  }
+};
+
+//-- updating job post by job id--
+const updateJob = async (req, res) => {
+  const jobId = req.params.id; // Get the job ID from the URL
+  const updatedData = req.body; // New data for the job post
+
+  try {
+    const updatedJob = await Job.findByIdAndUpdate(jobId, updatedData, { new: true });
+   console.log("update",updatedJob)
+    if (!updatedJob) {
+      return res.status(404).json({ message: 'Job post not found' });
+    }
+   console.log("update",updatedJob)
+
+    res.json(updatedJob);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+ 
+// --Apply to aa specific  job by id --
+const applyToJob = async (req, res) => {
+
+try {
+    const {jobId, applicantData } = req.body;
+    const {applicantName, resume, email, phone, address} = applicantData;
+
+    const jobApplication = new JobApplication({
+      jobId,
+      applicantName,
+      email,
+      phone,
+      address,
+      resume,
+    });
+
+    const savedApplication = await jobApplication.save();
+    //console.log(savedApplication)
+    res.status(201).json(savedApplication);
+  } catch (error) { 
+  //  console.log("error aply",error.message)
+    res.status(400).json({ message: error.message });
+  }
+};
+
+
+module.exports = {
+  createJob,
+  getJobs,
+  updateJob,
+  applyToJob,
+}; 
   if (company) filter.company = new RegExp(company, 'i');
   if (tags) filter.tags = { $in: tags.split(',') };
   if (experience) filter.experience = { $gte: parseInt(experience) };
