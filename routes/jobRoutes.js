8ded12c6e68a29c8758567943375cabee29ec180
wb420 <82988726+wb420@users.noreply.github.com>
2023-11-06 14:50:39 +0600

    Update jobRoutes.js

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const jobController = require('../controllers/jobController');
-const applicationController = require('../controllers/applicationController');
+
 
 //--Create a job
 router.post('/jobs', jobController.createJob);
 //get jobs
 router.get('/jobs', jobController.getJobs);
 //update job by job id
-router.put('/jobs/:id/update', jobUpdateController.updateJob);
+router.put('/jobs/:id/update', jobController.updateJob);
 
-// --Apply to a job
-router.post('/jobs/:id/apply', applicationController.applyToJob);
+// --Apply to a job by id
+router.post('/jobs/:id/apply', jobController.applyToJob);
 
 module.exports = router; 
